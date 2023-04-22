487c249d6cc6eecd365552f3bf4a3f0c0b5880cf
Aman Kumar <101081368+amanindian@users.noreply.github.com>
2023-04-22 12:38:47 +0530

    Make Server using express

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,15 @@
 const connectToMongo = require('./DataBase');
 connectToMongo();   
+
+//Express
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`You have connected with http://localhost:${port}`)
+})
