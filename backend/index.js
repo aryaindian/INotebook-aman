8a0a369f8ea8e0a12442c448f40de36e11928b6e
Aman Kumar <101081368+amanindian@users.noreply.github.com>
2023-05-01 16:20:24 +0530

    Creating thunder clint collection

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ connectToMongo();
 //Express
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 //If we have to use req.body which used in './routes/auth' then first of all we have to use a middlewere called "app.use(express.json())" which i written below 
 app.use(express.json())
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,39 +1,49 @@
-const express = require('express');
-const router = express.Router()
-const User = require('../models/User')
-const { body, validationResult } = require('express-validator');
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 //Create a user using: POST "/auth"
-router.post('/',
+router.post(
+    "/createuser",
     [
-        body('name', 'Enter a valid name atleast 3 characters').isLength({ min: 3 }),
-        body('email', 'Enter a valid and Unique E-Mail').isEmail(),
-        body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
+        body("name", "Enter a valid name atleast 3 characters").isLength({
+            min: 3,
+        }),
+        body("email", "Enter a valid and Unique E-Mail").isEmail(),
+        body("password", "Password must be atleast 5 characters").isLength({
+            min: 5,
+        }),
     ],
-    (req, res) => {
-        /*
-        // res.send("We are Current on the auth page see terminal to watch `req.body`")
-        res.send(req.body)  //Or we can write this to see in responce that what we send in body 
-        console.log(req.body); //This will log in terminal which we will send to body
+    async (req, res) => {
+        // Used Express Validator
 
-        const user = User(req.body) //Get body data accoding User Schema
-        user.save() //Save req.body data in MongoDB server
-        */
-
-        //Express Validator
+        // if there are no error return bad request and the errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        User.create({
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email,
-        }).then(user => res.json(user))
-            .catch(err => {
-                console.log(err)
-                res.json({ error: 'Please enter a unique value for email', message: err.message })
-            })
-    })
+
+        //check whether the user with the email exists or not
+        try {
+            let user = await User.findOne({ email: req.body.email });
+            if (user) {
+                return res
+                    .status(400)
+                    .json({ error: "User with the same email already exists" });
+            }
+            user = await User.create({
+                name: req.body.name,
+                password: req.body.password,
+                email: req.body.email,
+            });
+
+            res.json(user);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Some Error occured");
+        }
+    }
+);
 
 module.exports = router;
